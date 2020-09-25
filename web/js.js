b17d839a7b2a0751125119d16ca07fbf01d0e8d9
Alan S Mathew <alansmathew@Icloud.com>
2020-09-25 15:53:53 +0530

    with animation

--- web/js.js
+++ web/js.js
@@ -1,13 +1,34 @@
 var flag=true
-var deg=0
-function startanimation(){
-    deg+=180;
-    if(flag){
-        document.getElementById("back_of_input").style.transform = "translate(-50%, -50%) rotateY("+deg+"deg)";
+function startanimation(temp){
+    if(flag && temp=='next'){
+        document.getElementById("back_of_input").style.transform = " rotateY(180deg)";
+        document.getElementsByClassName("click_button")[1].style.width = "109px";
+        document.getElementsByClassName("click_button")[0].style.display = "inline";
+        document.getElementsByClassName("click_button")[0].style.width = "107px";
         flag=false
     }
-    else if ( flag==false){
-        document.getElementById("back_of_input").style.transform = "translate(-50%, -50%) rotateY("+deg+"deg)";
+    else if (flag==false && temp =='back'){
+        document.getElementById("back_of_input").style.transform = " rotateY(0deg)";
+        document.getElementsByClassName("click_button")[1].style.width = "220px";
+        document.getElementsByClassName("click_button")[0].style.display = "none";
         flag=true
     }
 }
+
+var balls = document.getElementsByClassName("ball");
+document.onmousemove = function(){
+  var x = event.clientX * 100 / window.innerWidth + "%";
+  var y = event.clientY * 100 / window.innerHeight + "%";
+
+  for(var i=0;i<2;i++){
+    balls[i].style.left = x;
+    balls[i].style.top = y;
+    balls[i].style.transform = "translate(-"+x+",-"+y+")";
+  }
+}
+
+// beer animation 
+// document.querySelector('body').addEventListener('mousemove',eyeball);
+// function eyeball(){
+
+// }

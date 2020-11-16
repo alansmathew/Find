67c06d03505221b80a63f7d2af85bdb9ba6e33d4
Alan S Mathew <alansmathew@Icloud.com>
2020-11-16 22:27:44 +0530

    register invalid field rectified

--- web/php/register_.php
+++ web/php/register_.php
@@ -1,21 +1,25 @@
 <?php
 include("connection.php");
+if(isset($_POST['name']) && isset($_POST['email']) && isset($_POST['password']))
+{
+    $name= $_POST['name'];
+    $email=$_POST['email'];
+    $password=$_POST['password'];
+    $password = password_hash($password,PASSWORD_DEFAULT); 
 
-$name= $_POST['name'];
-$email=$_POST['email'];
-$password=$_POST['password'];
-
-$password = password_hash($password,PASSWORD_DEFAULT); 
-
-$sql="insert into tbl_login (email,password) values ('$email','$password')";
-mysqli_query($con,$sql);
-$login_id=mysqli_insert_id($con);
-$sql="insert into tbl_reg (login_id,name) values($login_id,'$name')";
-if(mysqli_query($con,$sql)){
-    session_start();
-    $_SESSION['id']=$login_id;
-    header("Location:mapview.php");
-}else{
-    echo "something went wrong";
+    $sql="insert into tbl_login (email,password) values ('$email','$password')";
+    mysqli_query($con,$sql);
+    $login_id=mysqli_insert_id($con);
+    $sql="insert into tbl_reg (login_id,name) values($login_id,'$name')";
+    if(mysqli_query($con,$sql)){
+        session_start();
+        $_SESSION['id']=$login_id;
+        header("Location:mapview.php");
+    }else{
+        echo "something went wrong";
+    }
+}
+else{
+    header("Location:../index.html");
 }
 ?>

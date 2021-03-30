7b57d175e331d1e43f76e7f3d5ec4b74033b4970
alansmathew <alansmathew@mca.ajce.in>
2021-03-30 09:58:21 +0530

    just before 0th rewiew

--- web/php/forget_pass/pass_reset.php
+++ web/php/forget_pass/pass_reset.php
@@ -1,16 +1,39 @@
-<?php
-include("../connection.php");
-session_start();
-if( isset($_SESSION['login']) && isset($_POST['password'])){
-    $login=$_SESSION['login'];
-    $password=$_POST['password'];
-    $password = password_hash($password,PASSWORD_DEFAULT); 
-    $sql="UPDATE tbl_login SET password='$password' where login_id=$login";
-    mysqli_query($con,$sql);
-    session_unset();
-    if(session_destroy()){header("location:../../index.php");}
-}
-else{
-    header("location:../forget_pass.php");
-}
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reset Password</title>
+    <link rel="stylesheet" href="../../styles/forget_mail_style.css">
+</head>
+<body>
+    <?php
+        include("../connection.php");
+        session_start();
+        if( isset($_SESSION['login']) && isset($_POST['password'])){
+            $login=$_SESSION['login'];
+            $password=$_POST['password'];
+            $password = password_hash($password,PASSWORD_DEFAULT); 
+            $sql="UPDATE tbl_login SET password='$password' where login_id=$login";
+            mysqli_query($con,$sql);
+            session_unset();
+            if(session_destroy()){
+                ?>
+                    <center><img src="../../images/find_logo.png" alt=""></center>
+                    <div class="container">
+                        <h1>Reset Sucessfull </h1>
+                        <p>Your password is been sucessfully reseted. You can use your new password to login to your Find account.</p>
+                        <a href="../../index.php"><button class="click_button" > Continue to home page  </button></a>
+                    </div> 
+                <?php
+                // header("location:../../index.php");
+            }
+        }
+        else{
+            header("location:../forget_pass.php");
+        }
+    ?>
+
+</body>
+</html>

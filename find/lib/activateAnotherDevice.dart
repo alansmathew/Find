42a5bde72311b567de836cfc7c05af21ca48147c
Alan S Mathew <alansmathew@icloud.com>
2021-07-14 14:44:22 +0530

    major update with location Null on activateanother

--- find/lib/activateAnotherDevice.dart
+++ find/lib/activateAnotherDevice.dart
@@ -5,6 +5,7 @@ import 'package:find/pages.dart';
 import 'package:find/update_locAnd_Sound.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_blue/flutter_blue.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
 
@@ -21,7 +22,7 @@ class _activateAnotherDeviceState extends State<activateAnotherDevice> {
   var current_rssid = -100 ;
   var bluethoothId ;
   var hashname = '' ;
-  var Textdata = 'Activating another device\n';
+  var Textdata = 'Activating another device';
 
   getbluetoothdevices()  {
 
@@ -65,7 +66,7 @@ class _activateAnotherDeviceState extends State<activateAnotherDevice> {
     var dat = jsonDecode(responseBluetoothParing.body);
     if (dat['value'] == 'true'){
       setState(() {
-        Textdata = Textdata + dat['device'] ;
+        Textdata = "Offline finding activated for \n"+ dat['device'] ;
       });
     }
     else{
@@ -80,7 +81,14 @@ class _activateAnotherDeviceState extends State<activateAnotherDevice> {
     getbluetoothdevices() ;
     await new Future.delayed(const Duration(seconds : 7));
     print(current_rssid);
+    print("getting current location");
+    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    lat = position.latitude;
+    lon = position.longitude;
+      // this lattitude and longitude is stored inside globals.dart file and then imported to this file to get access !
+    print("lattitued : ${lat}, longitude : ${lon}");
     sentOfflinedata();
+    // print(lat.toString());
   }
 
 @override
--- find/lib/activateoffline.dart
+++ find/lib/activateoffline.dart
@@ -1,7 +1,9 @@
 
 import 'package:find/globals.dart';
+import 'package:find/map.dart';
 
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
 
@@ -33,6 +35,11 @@ class _activateofflineState extends State<activateoffline> {
     // var data = jsonDecode(response.body);
   }
 
+  void _goToMapView(){
+    print('going to mapview');
+    Get.to(Mapv());
+  }
+
   @override
   void initState() {
     setListning();
@@ -41,10 +48,81 @@ class _activateofflineState extends State<activateoffline> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Get closer to anther device \n Loading...",textAlign: TextAlign.center,),
-      ),
+        body:Center(
+          child: Container(
+            child: Column(
+              children: [
+                Image.asset('images/paring.jpg',height:MediaQuery.of(context).size.height * 0.35),
+                Spacer(),
+                Spacer(),
+                Container(
+                  child: Container(
+                    child: Column(
+                      children: [
+                        Spacer(),
+                        Text("Get Closser..",style: TextStyle(color: Colors.white,fontSize: 25),),
+                        Container(
+                          width: MediaQuery.of(context).size.width*0.75,
+                          child:Text("After seeing your device name on near by device, Click on proceed button to go to Map View",
+                            style: TextStyle(color: Colors.white70,fontSize: 14),
+                            textAlign: TextAlign.center,
+                          ),
+                          margin: const EdgeInsets.only(top: 7.0),
+                        ),
+                        Spacer(),
+                        Container(
+                          child: SizedBox(
+                            width: 200,
+                            height:40,
+                            child:ElevatedButton(
+                              child: Text(
+                                "Proceed".toUpperCase(),
+                                style: TextStyle(fontSize: 14,color: Colors.black),
+                              ),
+                              style: ButtonStyle(
+                                  foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                                  backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                                    RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(18.0),
+                                      side: BorderSide(color: Colors.white54),
+                                    ),
+                                  )
+                              ),
+                              onPressed:_goToMapView,
+                            ),
+                          ),
+                          margin: const EdgeInsets.only(bottom: 15.0),
+                        ),
+                        Spacer(),
+                      ],
+                    ),
+                  ),
+                  height:MediaQuery.of(context).size.height * 0.60,
+                  width: MediaQuery.of(context).size.width*0.98,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(30.0),
+                      topLeft: Radius.circular(30.0),
+                    ),
+                    color: Color.fromARGB(255,106,111,196),
+                    boxShadow: [
+                      BoxShadow(color: Colors.white, spreadRadius: 10),
+                    ],
+                  ),
+                ),
+                // Spacer()
+              ],
+            ),
+            color:Colors.white,
+          ),
+        )
     );
+    // return Scaffold(
+    //   body: Center(
+    //     child: Text("Get closer to anther device. After seeing your device name on nearby activating device, click Proceed",textAlign: TextAlign.center,),
+    //   ),
+    // );
   }
 }
 
